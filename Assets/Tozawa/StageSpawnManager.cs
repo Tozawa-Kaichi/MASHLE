ba43f9791c3d7914d7cf74981145d6aed59d079d
DESKTOP-26ND2P6\Owner <st37071556@icloul.com>
2022-08-23 15:59:48 +0900

    ステージ生成スクリプトを作成

---
+++ Assets/Tozawa/StageSpawnManager.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/// <summary>
+/// ステージの自動生成を管理するコンポーネント（スクリプト）
+/// </summary>
+public class StageSpawnManager : MonoBehaviour
+{
+    [Header("出てきてほしいステージのプレハブを入れてください"), Tooltip("生成するプレハブの配列"), SerializeField]
+    GameObject[] _stagePrefab = new GameObject[0];
+    [Header("ステージが生まれる場所をドラッグアンドドロップしてね"), Tooltip("スポーン場所のトランスフォーム"), SerializeField]
+    Transform _spawnPoint = default;
+    [Header("ステージが生成される間隔を調整してね"), Tooltip("スポーンインターバル"), SerializeField]
+    float _spawnInterval = 0;
+    int _random = default;
+    float _time = 0;
+    private void Start()
+    {
+        
+    }
+    private void Update()
+    {
+        StageSpawner();
+    }
+    public void StageSpawner()
+    {
+        _time += Time.deltaTime;
+        if (_time > _spawnInterval)
+        {
+            _random = Random.Range(0,_stagePrefab.Length);
+            Instantiate(_stagePrefab[_random],_spawnPoint.position,Quaternion.identity);
+            _time = 0;
+        }
+    }
+}
